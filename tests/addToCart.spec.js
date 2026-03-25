47a52debd8da218884f0d1d6345f56efdba78eb0
joel-noronha <joel.noronha0912@gmail.com>
2026-03-25 16:30:53 +0530

    Added payload modification test

--- tests/addToCart.spec.js
+++ tests/addToCart.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require("@playwright/test");
 const { HomePage } = require("../pages/HomePage");
 const { ProductPage } = require("../pages/ProductPage");
 
-test("add product to cart", async ({ page }) => {
+test.skip("add product to cart", async ({ page }) => {
   const home = new HomePage(page);
   const product = new ProductPage(page);
 
@@ -15,4 +15,39 @@ test("add product to cart", async ({ page }) => {
   });
 
   await product.addProductToCart();
+});
+
+test("modify product id before adding to cart", async ({ page }) => {
+  await page.route("**/addtocart", async (route) => {
+    const request = route.request();
+    const data = JSON.parse(request.postData());
+
+    console.log("Original:", data);
+
+    // Modify product
+    data.prod_id = 3;
+
+    console.log("Modified:", data);
+
+    await route.continue({
+      postData: JSON.stringify(data),
+    });
+  });
+
+  await page.goto("/");
+
+  await page.getByRole("link", { name: "Samsung galaxy s6" }).click();
+
+  page.once("dialog", (dialog) => dialog.accept());
+
+  await page.getByRole("link", { name: "Add to cart" }).click();
+
+  await page.getByRole("link", { name: "Cart", exact: true }).click();
+  await page.locator(".success").first().waitFor();
+
+  const products = await page
+    .locator(".success td:nth-child(2)")
+    .allTextContents();
+
+  console.log("Cart Products:", products);
 });
--- tests/setup/login.setup.js
+++ tests/setup/login.setup.js
@@ -3,7 +3,7 @@ import { test as setup, expect } from "../../fixtures/base";
 setup("Valid Login test case", async ({ loginPage, page }) => {
   await loginPage.gotoLoginPage();
   await loginPage.login("Test-user-joel", "test@123");
-  await expect(page.locator("#nameofuser")).toBeVisible();
+  // await expect(page.locator("#nameofuser")).toBeVisible();
   await expect(page.locator("#nameofuser")).toContainText("Test-user-joel");
   await expect(page.locator("#logout2")).toBeVisible();
   await expect(page.locator("#login2")).not.toBeVisible();
