e409d6e8ba58512f46ab03ce12aba8a485d6e04f
joel-noronha <joel.noronha0912@gmail.com>
2026-03-27 13:02:26 +0530

    login setup assertion fixed

--- tests/setup/login.setup.js
+++ tests/setup/login.setup.js
@@ -4,7 +4,9 @@ setup("Valid Login test case", async ({ loginPage, page }) => {
   await loginPage.gotoLoginPage();
   await loginPage.login("Test-user-joel", "test@123");
   // await expect(page.locator("#nameofuser")).toBeVisible();
-  await expect(page.locator("#nameofuser")).toContainText("Test-user-joel");
+  await expect(page.locator("#nameofuser")).toHaveText(
+    "Welcome Test-user-joel",
+  );
   await expect(page.locator("#logout2")).toBeVisible();
   await expect(page.locator("#login2")).not.toBeVisible();
   await expect(page).toHaveURL("https://demoblaze.com/");
